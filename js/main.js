b71807b34c47e6eb6fbad54f8b5b1125853cc402
Shinoob09 <121316753+Shinoob09@users.noreply.github.com>
2025-06-01 12:29:11 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+import { inicializarCarrinho } from './carrinho.js';
+import { renderizarProdutos } from './produtos.js';
+import { configurarModal } from './modal.js';
+import { configurarPedido } from './pedido.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderizarProdutos();
+  inicializarCarrinho();
+  configurarModal();
+  configurarPedido();
+});
