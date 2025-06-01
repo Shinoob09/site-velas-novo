29449a4597276a289ea999b56b14fa45c103b5b0
Shinoob09 <121316753+Shinoob09@users.noreply.github.com>
2025-06-01 12:24:33 -0300

    Delete verdi_corrigido (1)/public directory

--- verdi_corrigido (1)/public/js/dom-utils.js
+++
@@ -1,8 +0,0 @@
-
-export const getEl = (id) => document.getElementById(id) || null;
-
-export function sanitizeInput(input) {
-  const div = document.createElement("div");
-  div.textContent = input;
-  return div.innerHTML;
-}
